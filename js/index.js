acd2de17efd7b8e05daf9cb3da7d211916723b68
Tom Arns <tomrarns@gmail.com>
2024-05-07 15:08:20 -0700

    tried aync

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
 // fetch all the people from the Star Wars API (SWAPI)
 //   Add all the people and thgeir basic info to the container in the people section
-// fetch one page
-const baseUrl = "https://swapi.dev/api/people?page=";
-const recordLength = 9;
+
+// initial fetch to get record count
+const baseURL = "https://www.swapi.tech/api/people";
+let recordLength = 0;
+async function getRecordCount(baseURL) {
+    await fetch(baseURL)
+        .then((response) => {
+            if (response.ok) {
+                return response.text();
+            } else {
+                throw new Error("Failed to fetch initial record");
+            }
+        })
+        .then((data) => {
+            const peoplePage = JSON.parse(data);
+            console.log(peoplePage);
+            return recordLength = peoplePage.total_pages;
+        })
+        .catch((error) => {
+            if (error instanceof SyntaxError) {
+                console.error("Unparsable response from server");
+            } else {
+                console.error("Error fetching data: ", error.message);
+            }
+        });
+}
+getRecordCount(baseURL).then(res => {recordLength = res});
+console.log("recordLength: ", recordLength);
+
+const pageUrl = baseURL + "?page=";
 const urls = [];
 const peopleContainer = document.getElementById('people-container')
 for (let i = 0; i < recordLength; i++) {
-    urls.push(baseUrl + (i + 1));
+    urls.push(pageUrl + (i + 1));
 }
 async function getAllPages() {
     const promiseList = urls.map(text => fetch(text).then(r => r.json().catch(err => console.log(err))));
