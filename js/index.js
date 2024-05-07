4b45ec0a35ba6f2e438da2cc995fd5d275448ac8
Tom Arns <tomrarns@gmail.com>
2024-05-07 14:47:44 -0700

    get all pages, need to fix hard coded page count

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
 // fetch all the people from the Star Wars API (SWAPI)
 //   Add all the people and thgeir basic info to the container in the people section
 // fetch one page
-url = "https://www.swapi.tech/api/people";
-const peopleContainer = document.getElementById("people-container");
-fetch(url)
-    .then((response) => {
-        if (response.ok) {
-            return response.text();
-        } else {
-            throw new Error("Failed to fetch repositories");
-        }
-    })
-    .then((data) => {
-        const peoplePage = JSON.parse(data);
-        console.log(peoplePage);
-        // Create a new element for each person
-        for (let person of peoplePage.results) {
+const baseUrl = "https://swapi.dev/api/people?page=";
+const recordLength = 9;
+const urls = [];
+const peopleContainer = document.getElementById('people-container')
+for (let i = 0; i < recordLength; i++) {
+    urls.push(baseUrl + (i + 1));
+}
+async function getAllPages() {
+    const promiseList = urls.map(text => fetch(text).then(r => r.json().catch(err => console.log(err))));
+    const finalResult = await Promise.all(promiseList).then(result => {
+        let finalList = []
+        result.forEach(res => {
+            finalList = finalList.concat(res.results);
+        });
+        console.log("finalList: ", finalList);
+        for (let person of finalList) {
             let personElt = document.createElement("div");
             personElt.className = 'person';
             // add a header with the person's name
@@ -24,13 +25,14 @@ fetch(url)
             personElt.appendChild(personHeader);
             peopleContainer.appendChild(personElt);
         }
-    })
-    .catch((error) => {
-        if (error instanceof SyntaxError) {
-            console.error("Unparsable response from server");
-        } else {
-            console.error("Error fetching data: ", error.message);
-        }
+        return finalList
     });
+    //console.log(finalResult);
+    //console.log(finalResult.length);
+}
+getAllPages();
+
+
+
 
 
